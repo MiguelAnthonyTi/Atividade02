1669b0f2cf9c5a7b6eeabda584e278454cb9e95c
MiguelAnthonyTi <125111410238@ulife.com.br>
2025-04-16 14:25:48 -0300

    Tarefa01

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int valor=1;
+
+    
+        while (valor<=50) {
+            System.out.println(valor);
+            valor ++; 
+            //O ++, ele aumenta o valor da variável em 1 a cada repetição se fosse -- ele diminuiria 
+        }
+
+
+    }
+}
